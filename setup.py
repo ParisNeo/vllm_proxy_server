53d366858906021a1ea6747084969575b7f3ed78
saloui <saifeddine.aloui@cea.fr>
2024-03-15 12:59:51 +0100

    upgraded

--- setup.py
+++ setup.py
@@ -17,21 +17,21 @@ requirements_dev = read_requirements("requirements_dev.txt")
 
 
 setuptools.setup(
-    name="ollama_proxy_server",
+    name="vllm_proxy_server",
     version="7.1.0",
     author="Saifeddine ALOUI (ParisNeo)",
     author_email="aloui.saifeddine@gmail.com",
     description="A fastapi server for petals decentralized text generation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ParisNeo/ollama_proxy_server",
+    url="https://github.com/ParisNeo/vllm_proxy_server",
     packages=setuptools.find_packages(),  
     include_package_data=True,
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'ollama_proxy_server = ollama_proxy_server.main:main',
-            'ollama_proxy_add_user = ollama_proxy_server.add_user:main',
+            'vllm_proxy_server = vllm_proxy_server.main:main',
+            'vllm_proxy_add_user = vllm_proxy_server.add_user:main',
         ],
     },
     extras_require={"dev": requirements_dev},
